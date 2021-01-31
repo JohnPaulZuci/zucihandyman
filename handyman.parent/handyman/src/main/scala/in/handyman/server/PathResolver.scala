de51fe635f24633a69cae1e2b36d3030eba9430d
Zuci Admin <Zuci_Admin@stategroup.com>
2021-01-31 10:46:07 -0500

    added worrkaround for path

--- handyman.parent/handyman/src/main/scala/in/handyman/server/PathResolver.scala
+++ handyman.parent/handyman/src/main/scala/in/handyman/server/PathResolver.scala
@@ -1,12 +1,42 @@
 package in.handyman.server
 
-object PathResolver {
-  
-  def resolvePath(processName:String, fileRelativePath:String, basePath:String):String={
-    
+import com.typesafe.scalalogging.LazyLogging
+import org.slf4j.MarkerFactory
+
+object PathResolver extends LazyLogging {
+
+  val marker = "PROCESS-PATH RESOLVER";
+  val pMarker = MarkerFactory.getMarker(marker)
+
+  def resolvePath(processName: String, fileRelativePath: String, basePath: String): String = {
+    /* val tempPath = {
+      basePath.contains("file:")
+    }
     val fullPath = new StringBuilder().append(basePath).
-    append("/").append(fileRelativePath).toString()
-    println("fullpath="+fullPath)
+      append("/").append(fileRelativePath).toString()
+    println("fullpath=" + fullPath)
+    val finalPath = {
+      if (basePath.contains("file:")) {
+
+          val basePath1 = basePath.substring("file:".length(),basePath.length())
+          val fullPath = new StringBuilder().append(basePath1).
+          append("/").append(fileRelativePath).toString()
+          logger.info(pMarker, "fullpath {} for process {} with base path as {}" , fullPath,processName , basePath1)
+        ""
+
+      } else {
+
+        val fullPath = new StringBuilder().append(basePath).
+          append("/").append(fileRelativePath).toString()
+        logger.info(pMarker, "fullpath {} for process {} with base path as {}" , fullPath,processName , basePath)
+        this.getClass.getClassLoader.getResource(fullPath).getPath
+      }
+    }
+    finalPath*/
+
+    val fullPath = new StringBuilder().append(basePath).
+      append("/").append(fileRelativePath).toString()
+    println("fullpath=" + fullPath)
     val finalPath = this.getClass.getClassLoader.getResource(fullPath).getPath
     finalPath
   }
