1c495ee66960e6f6f3c524c7d914bfd44e0b1bbc
Thamarai Selvi <thamaraiselvi.p@zucisystems.com>
2021-11-08 17:00:48 +0530

    body split and data ingestion enhancement

--- handyman.parent/handyman/src/main/scala/in/handyman/process/onethread/Mongo2DbAction.scala
+++ handyman.parent/handyman/src/main/scala/in/handyman/process/onethread/Mongo2DbAction.scala
@@ -13,10 +13,6 @@ import org.bson.Document
 import org.json.JSONArray
 import org.json.JSONObject
 
-import com.google.common.reflect.TypeToken
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonArray
 import com.mongodb.BasicDBObject
 import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoClient
@@ -289,18 +285,16 @@ class Mongo2DbAction extends in.handyman.command.Action with LazyLogging {
       } else if ((colVal.isInstanceOf[java.util.ArrayList[_]])) {
         val colValObj = colVal.asInstanceOf[java.util.ArrayList[_]]
         if (colValObj != null && !colValObj.isEmpty()) {
-          val colva = colValObj.asInstanceOf[java.util.ArrayList[Document]]
-          
-          val gson : Gson = new GsonBuilder().create();
+          /*val gson : Gson = new GsonBuilder().create();
           val myCustomArray : JsonArray = gson.toJsonTree(colValObj).getAsJsonArray();
           System.out.println("array element4....."+ myCustomArray.toString());
           System.out.println("array element5....."+ gson.toJson(colValObj));
-          System.out.println("array element6....."+ gson.toJson(colValObj, new TypeToken[ArrayList[_]]() {}.getType()))
+          System.out.println("array element6....."+ gson.toJson(colValObj, new TypeToken[ArrayList[_]]() {}.getType()))*/
 
           val jsonArray : JSONArray = new JSONArray(colValObj);
-          logger.info("array element1....."+jsonArray.toString());
+          /*logger.info("array element1....."+jsonArray.toString());
           System.out.println("array element2....."+(new JSONArray(colValObj.toArray())).toString());
-          System.out.println("array element3....."+(new JSONArray(colValObj.toArray())).toString());
+          System.out.println("array element3....."+(new JSONArray(colValObj.toArray())).toString());*/
           /*colValStr = ""
           colValObj.forEach(colValO => colValStr = colValStr + colValO.toString() + "|")
           queryAppend = queryAppend + "\'" + colValStr.substring(0, colValStr.length()-1) + "\'" + ","*/
