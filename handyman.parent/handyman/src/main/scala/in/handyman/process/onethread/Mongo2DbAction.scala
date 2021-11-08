ca8e7e038e0b188ea361daaaf7671c4fed6cd478
Thamarai Selvi <thamaraiselvi.p@zucisystems.com>
2021-11-08 12:35:33 +0530

    insert email_verification_sorad_application_data

--- handyman.parent/handyman/src/main/scala/in/handyman/process/onethread/Mongo2DbAction.scala
+++ handyman.parent/handyman/src/main/scala/in/handyman/process/onethread/Mongo2DbAction.scala
@@ -13,7 +13,6 @@ import org.bson.Document
 import org.json.JSONArray
 import org.json.JSONObject
 
-import com.google.gson.Gson
 import com.mongodb.BasicDBObject
 import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoClient
@@ -286,9 +285,9 @@ class Mongo2DbAction extends in.handyman.command.Action with LazyLogging {
         val colValObj = colVal.asInstanceOf[java.util.ArrayList[_]]
         if (colValObj != null && !colValObj.isEmpty()) {
           val jsonArray : JSONArray = new JSONArray(colValObj);
-          /*logger.debug("array element....."+jsonArray.toString());
           logger.info("array element1....."+jsonArray.toString());
-          System.out.println("array element2....."+jsonArray.toString());*/
+          System.out.println("array element2....."+(new JSONArray(colValObj.toArray())).toString());
+          System.out.println("array element3....."+(new JSONArray(colValObj.toArray())).toString());
           /*colValStr = ""
           colValObj.forEach(colValO => colValStr = colValStr + colValO.toString() + "|")
           queryAppend = queryAppend + "\'" + colValStr.substring(0, colValStr.length()-1) + "\'" + ","*/
